b4adb41a232d71e9994645158741a707a9b4d395
Samra Girma <samra.girma@gmail.com>
2021-11-16 14:22:51 -0700

    Add chapter 2 exercises

---
+++ 2_variables/name.rb
@@ -0,0 +1,23 @@
+#LS Intro to Ruby
+#Chapter 2: Variables
+#Exercises 1, 3, 4
+
+#Exercise 1
+puts "Please type your first name: "
+first_name = gets.chomp
+
+puts "Why, hello there #{first_name}! Lovely to meet you!"
+
+#Exercise 3
+10.times { puts first_name }
+
+#Exercise 4
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Your full name is #{first_name} #{last_name}"
+
+
